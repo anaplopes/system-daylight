1735780bbd55b5c64a3b832e3912f47caa0e542b
Ana Paula <30847848+AnaToco@users.noreply.github.com>
2018-07-28 22:59:14 -0300

    correção de bug com fontawesome

--- core/views/lista.py
+++ core/views/lista.py
@@ -4,5 +4,5 @@ from core.models import *
 
 
 def list_usuario(request):
-    lista_usuario = Usuario.objects.filter(nome__contains='ana')
+    lista_usuario = Usuario.objects.filter(nome__contains="nome")
     return render(request, "grupo/grupoUsuario.html", { 'lista_usuario' : lista_usuario })
