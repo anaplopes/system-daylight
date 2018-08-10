3f0506deb0dcb49a661db8c6e99e08ee07200a40
Ana Paula Lopes <30847848+AnaToco@users.noreply.github.com>
2018-08-10 18:03:51 -0300

    add funções update
    add funções update_fornecedor, update_prestador, update_material, update_produto, update_servico

--- core/views/atualizar.py
+++ core/views/atualizar.py
@@ -23,3 +23,58 @@ def update_cliente(request, id):
         return redirect('list_cliente')
 
     return render(request, 'cadastro/cliente.html', { 'form' : form, 'update_cliente':update_cliente })
+
+
+def update_fornecedor(request, id):
+    update_fornecedor = Fornecedor.objects.get(id=id)
+    form = FornecedorForm(request.POST or None, instance=update_fornecedor)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_fornecedor')
+
+    return render(request, 'cadastro/fornecedor.html', { 'form' : form, 'update_fornecedor':update_fornecedor })
+
+
+def update_prestador(request, id):
+    update_prestador = PrestadorServico.objects.get(id=id)
+    form = PrestadorForm(request.POST or None, instance=update_prestador)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_prestador')
+
+    return render(request, 'cadastro/prestador.html', { 'form' : form, 'update_prestador':update_prestador })
+
+
+def update_material(request, id):
+    update_material = Material.objects.get(id=id)
+    form = MaterialForm(request.POST or None, instance=update_material)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_material')
+
+    return render(request, 'cadastro/material.html', { 'form' : form, 'update_material':update_material })
+
+
+def update_produto(request, id):
+    update_produto = Produto.objects.get(id=id)
+    form = ProdutoForm(request.POST or None, instance=update_produto)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_produto')
+
+    return render(request, 'cadastro/produto.html', { 'form' : form, 'update_produto':update_produto })
+
+
+def update_servico(request, id):
+    update_servico = Servico.objects.get(id=id)
+    form = ServicoForm(request.POST or None, instance=update_servico)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_servico')
+
+    return render(request, 'cadastro/servico.html', { 'form' : form, 'update_servico':update_servico })
