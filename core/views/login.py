f1ac78bdb95f01dcb01bb3eb3249d0edf46ea132
Ana Paula Lopes <30847848+AnaToco@users.noreply.github.com>
2018-08-29 09:44:21 -0300

    add função login

--- core/views/login.py
+++ core/views/login.py
@@ -1,7 +1,36 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import redirect
+from django.views.generic import View
+from django.http import HttpResponse
+import django.contrib.messages as messages
 from django.shortcuts import render
 from core.forms import *
 from core.models import *
 
 
-def login(request):
-    return render(request, "login.html")
+class AcessoView(View):
+    def login(self):
+        context = {}
+        next = self.GET.get('next')
+        if self.method == 'POST':
+            username = self.POST.get('email')
+            senha = self.POST.get('password')
+            usuario = authenticate(username=username, password=senha)
+
+            if usuario is not None:
+                login(self, usuario)
+                if next:
+                    return redirect(next)
+                return redirect('home')
+
+            messages.error(self, 'Login e senha não encontrados. Tente logar novamente!', "Login inválido")
+            return redirect('login')
+        template = 'login.html'
+        return HttpResponse(template.render(context, self))
+    
+    
+    def logout(self):
+        logout(self)
+        return redirect('login')
+    
