f9477f9704fc42211d15e538793098838b2ec736
Ana Paula Lopes <30847848+AnaToco@users.noreply.github.com>
2018-08-30 21:55:24 -0300

    Update urls.py

--- daylightproject/urls.py
+++ daylightproject/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
 
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('accounts/', include('core.urls')),
     path('admin/', admin.site.urls),
-    ]
+    path('', include('core.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
