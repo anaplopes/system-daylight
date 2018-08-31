dcece02e1332758628aa05f598f311e588a57886
Ana Paula Lopes <30847848+AnaToco@users.noreply.github.com>
2018-08-31 10:09:43 -0300

    add config media

--- daylightproject/settings.py
+++ daylightproject/settings.py
@@ -134,3 +134,7 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 AUTH_USER_MODEL = 'core.Usuario'
 LOGIN_REDIRECT_URL = 'home'
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
