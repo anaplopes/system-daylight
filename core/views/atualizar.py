c21716eae79a7b376a04e7687aa34121ba4fac28
Ana Paula Lopes <30847848+AnaToco@users.noreply.github.com>
2018-08-31 17:27:55 -0300

    Update atualizar.py

--- core/views/atualizar.py
+++ core/views/atualizar.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from core.forms import *
 from core.models import *
