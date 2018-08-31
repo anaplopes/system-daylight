aad5ec113da7ba9cde9d2abfb59085b320b289b8
Ana Paula Lopes <30847848+AnaToco@users.noreply.github.com>
2018-08-31 17:45:03 -0300

    Update usuario.py

--- core/models/usuario.py
+++ core/models/usuario.py
@@ -7,29 +7,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 PERFIL_CHOICES = (('G', 'Gerente'), ('A', 'Assistente'), ('V', 'Vendedor'),)
 
 ###################### Criação e administração do usuário e superuser.
-class UsuarioManager(BaseUserManager):
-    
-    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
-        now = timezone.now()
-        if not email:
-            raise ValueError(_('Email precisa ser preenchido'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, is_staff=is_staff, last_login=now, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email=None, password=None, **extra_fields):
-        return self._create_user(email, password, False, False, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        user=self._create_user(email, password, True, True, **extra_fields)
-        user.is_active=True
-        user.save(using=self._db)
-        return user
-
-
-class Usuario(AbstractBaseUser):
+class Usuario(AbstractBaseUser, PermissionsMixin):
     nome = models.CharField('Nome', max_length=50, null=False, blank=False)
     email = models.EmailField('E-mail', max_length=100, unique=True, null=False, blank=False)
     cpf = models.CharField('CPF', max_length=11, unique=True, null=False, blank=False)
