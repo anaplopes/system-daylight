438c97796d6b2ae6300eb4e5125cfa3afad3622e
Ana Paula Lopes <30847848+AnaToco@users.noreply.github.com>
2018-08-31 17:23:11 -0300

    add necessidade de login

--- core/views/atualizar.py
+++ core/views/atualizar.py
@@ -3,6 +3,7 @@ from core.forms import *
 from core.models import *
 
 
+@login_required(login_url='/login/')
 def update_usuario(request, id):
     update_usuario = Usuario.objects.get(id=id)
     form = UsuarioForm(request.POST or None, instance=update_usuario)
@@ -12,6 +13,7 @@ def update_usuario(request, id):
     return render(request, 'gerencial/cadastrarusuario.html', { 'form' : form, 'update_usuario':update_usuario })
 
 
+@login_required(login_url='/login/')
 def update_cliente(request, id):
     update_cliente = Cliente.objects.get(id=id)
     form = ClienteForm(request.POST or None, instance=update_cliente)
@@ -21,6 +23,7 @@ def update_cliente(request, id):
     return render(request, 'comercial/cadastrarcliente.html', { 'form' : form, 'update_cliente':update_cliente })
 
 
+@login_required(login_url='/login/')
 def update_fornecedor(request, id):
     update_fornecedor = Fornecedor.objects.get(id=id)
     form = FornecedorForm(request.POST or None, instance=update_fornecedor)
@@ -30,6 +33,7 @@ def update_fornecedor(request, id):
     return render(request, 'comercial/cadastrarfornecedor.html', { 'form' : form, 'update_fornecedor':update_fornecedor })
 
 
+@login_required(login_url='/login/')
 def update_prestador(request, id):
     update_prestador = PrestadorServico.objects.get(id=id)
     form = PrestadorForm(request.POST or None, instance=update_prestador)
@@ -39,6 +43,7 @@ def update_prestador(request, id):
     return render(request, 'producao/cadastrarprestador.html', { 'form' : form, 'update_prestador':update_prestador })
 
 
+@login_required(login_url='/login/')
 def update_material(request, id):
     update_material = Material.objects.get(id=id)
     form = MaterialForm(request.POST or None, instance=update_material)
@@ -48,6 +53,7 @@ def update_material(request, id):
     return render(request, 'comercial/cadastrarmaterial.html', { 'form' : form, 'update_material':update_material })
 
 
+@login_required(login_url='/login/')
 def update_medida(request, id):
     update_medida = UnidadeMedida.objects.get(id=id)
     form = UnidadeMedidaForm(request.POST or None, instance=update_medida)
@@ -57,6 +63,7 @@ def update_medida(request, id):
     return render(request, 'comercial/cadastrarmedida.html', { 'form' : form, 'update_medida':update_medida })
 
 
+@login_required(login_url='/login/')
 def update_produto(request, id):
     update_produto = Produto.objects.get(id=id)
     form = ProdutoForm(request.POST or None, instance=update_produto)
@@ -66,6 +73,7 @@ def update_produto(request, id):
     return render(request, 'comercial/cadastrarproduto.html', { 'form' : form, 'update_produto':update_produto })
 
 
+@login_required(login_url='/login/')
 def update_servico(request, id):
     update_servico = Servico.objects.get(id=id)
     form = ServicoForm(request.POST or None, instance=update_servico)
