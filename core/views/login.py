adbfeb98db386670b7026ea2f2dc32cc3519de78
Ana Paula Lopes <30847848+AnaToco@users.noreply.github.com>
2018-09-03 10:35:45 -0300

    change parameter in authenticate method to username

--- core/views/login.py
+++ core/views/login.py
@@ -12,7 +12,7 @@ def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, email=username, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None and user.is_active:
             login(request, user)
             return redirect('home')
