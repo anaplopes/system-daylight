72ceeed5d2cefac6f3988d6cc8d8bb1d11479b03
Ana Paula Lopes <30847848+anaplopes@users.noreply.github.com>
2019-11-30 19:53:02 -0300

    Update settings.py

--- daylightproject/settings.py
+++ daylightproject/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'daylightproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'daylightsystem',
-#        'USER': 'root',
-#        'PASSWORD': 'root',
-#        'HOST': 'localhost',
-#		'PORT': '3306',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-        }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'daylightsystem',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+		'PORT': '3306',
+    }
+}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        }
+#        }
 
 
 # Password validation
